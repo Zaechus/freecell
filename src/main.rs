f3cdaff4e94542fc3ed01234b49d50471a1139f1
Zaechus <zaechus@pm.me>
2023-09-14 12:56:17 -0600

    different approach

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::io::{stdout, Write};
 
 use crossterm::{
     cursor,
-    event::{self, Event, KeyCode},
-    execute,
+    event::{self, Event, KeyCode, KeyEvent},
+    execute, queue,
     style::{Color, Print, Stylize},
     terminal::{self, disable_raw_mode, enable_raw_mode},
     tty::IsTty,
@@ -19,47 +19,49 @@ fn main() {
     ];
     deck.shuffle(&mut thread_rng());
 
-    let mut freecells = ["   ", "   ", "   ", "   "];
-    let ace_piles = ["   ", "   ", "   ", "   "];
-
-    let mut cascades: [Vec<&str>; 8] = [
-        Vec::new(),
-        Vec::new(),
-        Vec::new(),
-        Vec::new(),
-        Vec::new(),
-        Vec::new(),
-        Vec::new(),
-        Vec::new(),
+    let mut cards: [Vec<&str>; 8] = [
+        vec!["   "],
+        vec!["   "],
+        vec!["   "],
+        vec!["   "],
+        vec!["   "],
+        vec!["   "],
+        vec!["   "],
+        vec!["   "],
     ];
 
     for (i, card) in deck.drain(..).enumerate() {
-        cascades[i % 8].push(card);
+        cards[i % 8].push(card);
     }
 
     let mut cursor_pos: (u16, u16) = (0, 0);
 
     enable_raw_mode().unwrap();
     let mut stdout = stdout();
-    execute!(stdout, terminal::Clear(terminal::ClearType::All),).unwrap();
+    queue!(stdout, terminal::Clear(terminal::ClearType::All),).unwrap();
 
     'outer: loop {
-        execute!(stdout, terminal::Clear(terminal::ClearType::All),).unwrap();
-        execute!(stdout, cursor::MoveTo(0, 0)).unwrap();
+        queue!(
+            stdout,
+            terminal::Clear(terminal::ClearType::All),
+            cursor::MoveTo(0, 0)
+        )
+        .unwrap();
 
         print!("\t");
-        for x in freecells {
-            print!("{}\t", x)
+        for col in &cards {
+            if stdout.is_tty() {
+                print!("{}\t", col[0].with(card_color(col[0])))
+            } else {
+                print!("{}\t", col[0])
+            }
         }
-        for x in ace_piles {
-            print!("{}\t", x)
-        }
-        write!(stdout, "\r\n\r\n").unwrap();
+        print!("\r\n\r\n");
 
-        let longest_len = cascades.iter().max_by_key(|col| col.len()).unwrap().len();
-        for i in 0..longest_len {
+        let longest_len = cards.iter().max_by_key(|col| col.len()).unwrap().len();
+        for i in 1..longest_len {
             print!("\t");
-            for cascade in &cascades {
+            for cascade in &cards {
                 let s = cascade.get(i).unwrap_or(&"   ");
                 if stdout.is_tty() {
                     print!("{}\t", s.with(card_color(s)));
@@ -67,50 +69,49 @@ fn main() {
                     print!("{}\t", s)
                 }
             }
-            write!(stdout, "\r\n").unwrap();
+            print!("\r\n");
         }
+        stdout.flush().unwrap();
 
-        let mut vertical_max = (cascades[cursor_pos.0 as usize].len() + 1) as u16;
+        let mut vertical_max = (cards[cursor_pos.0 as usize].len() + 1) as u16;
         loop {
-            execute!(stdout, cursor::MoveTo(8 + cursor_pos.0 * 8, cursor_pos.1)).unwrap();
+            execute!(
+                stdout,
+                cursor::MoveTo(
+                    8 + cursor_pos.0 * 8,
+                    if cursor_pos.1 == 0 {
+                        0
+                    } else {
+                        cursor_pos.1 + 1
+                    }
+                )
+            )
+            .unwrap();
 
-            let event = event::read().unwrap();
-
-            if event == Event::Key(KeyCode::Char('q').into()) {
-                quit()
-            } else if event == Event::Key(KeyCode::Up.into())
-                || event == Event::Key(KeyCode::Char('k').into())
-            {
-                cursor_pos.1 = cursor_pos.1.saturating_sub(1);
-                if cursor_pos.1 == 1 {
-                    cursor_pos.1 = 0;
+            if let Event::Key(KeyEvent { code, .. }) = event::read().unwrap() {
+                match code {
+                    KeyCode::Char('h') | KeyCode::Left => {
+                        cursor_pos.0 = cursor_pos.0.saturating_sub(1).clamp(0, 7);
+                        vertical_max = (cards[cursor_pos.0 as usize].len() + 1) as u16;
+                    }
+                    KeyCode::Char('j') | KeyCode::Down => {
+                        cursor_pos.1 = cursor_pos.1.saturating_add(1);
+                    }
+                    KeyCode::Char('k') | KeyCode::Up => {
+                        cursor_pos.1 = cursor_pos.1.saturating_sub(1);
+                    }
+                    KeyCode::Char('l') | KeyCode::Right => {
+                        cursor_pos.0 = cursor_pos.0.saturating_add(1).clamp(0, 7);
+                        vertical_max = (cards[cursor_pos.0 as usize].len() + 1) as u16;
+                    }
+                    KeyCode::Char('q') => quit(),
+                    KeyCode::Esc => continue 'outer,
+                    KeyCode::Char(' ') | KeyCode::Enter => break,
+                    _ => (),
                 }
-            } else if event == Event::Key(KeyCode::Down.into())
-                || event == Event::Key(KeyCode::Char('j').into())
-            {
-                cursor_pos.1 = cursor_pos.1.saturating_add(1);
-                if cursor_pos.1 == 1 {
-                    cursor_pos.1 = 2;
-                }
-            } else if event == Event::Key(KeyCode::Left.into())
-                || event == Event::Key(KeyCode::Char('h').into())
-            {
-                cursor_pos.0 = cursor_pos.0.saturating_sub(1).clamp(0, 7);
-                vertical_max = (cascades[cursor_pos.0 as usize].len() + 1) as u16;
-            } else if event == Event::Key(KeyCode::Right.into())
-                || event == Event::Key(KeyCode::Char('l').into())
-            {
-                cursor_pos.0 = cursor_pos.0.saturating_add(1).clamp(0, 7);
-                vertical_max = (cascades[cursor_pos.0 as usize].len() + 1) as u16;
-            } else if event == Event::Key(KeyCode::Esc.into()) {
-                continue 'outer;
-            } else if event == Event::Key(KeyCode::Enter.into())
-                || event == Event::Key(KeyCode::Char(' ').into())
-            {
-                break;
             }
 
-            cursor_pos.1 = cursor_pos.1.clamp(0, vertical_max);
+            cursor_pos.1 = cursor_pos.1.clamp(0, vertical_max - 2);
         }
         if cursor_pos.1 == 0 && cursor_pos.0 > 3 {
             continue;
@@ -119,7 +120,14 @@ fn main() {
 
         execute!(
             stdout,
-            cursor::MoveTo((cursor_pos.0 + 1) * 8 - 1, cursor_pos.1),
+            cursor::MoveTo(
+                (cursor_pos.0 + 1) * 8 - 1,
+                if cursor_pos.1 == 0 {
+                    0
+                } else {
+                    cursor_pos.1 + 1
+                }
+            ),
             Print("["),
             cursor::MoveToColumn((cursor_pos.0 + 1) * 8 + 4),
             Print("]"),
@@ -164,43 +172,18 @@ fn main() {
             }
         }
         let place_column = cursor_pos.0 as usize;
-
-        if pick_pos.1 == 0
-            && top
-            && pick_pos.0 < 4
-            && place_column < 4
-            && freecells[place_column] == "   "
-        {
-            let tmp = freecells[pick_pos.0];
-            freecells[pick_pos.0] = "   ";
-            freecells[place_column] = tmp;
-        } else if pick_pos.0 < 4
-            && can_place(
-                freecells[pick_pos.0],
-                cascades[place_column].last().unwrap(),
-            )
-        {
-            let tmp = freecells[pick_pos.0];
-            freecells[pick_pos.0] = "   ";
-            cascades[place_column].push(tmp);
-        } else {
-            let pick_pos = (pick_pos.0, pick_pos.1 - 2);
-            if top && place_column < 4 && freecells[place_column] == "   " {
-                let card = cascades[pick_pos.0].remove(pick_pos.1);
-                freecells[place_column] = card;
-            } else if can_place(
-                cascades[pick_pos.0][pick_pos.1],
-                cascades[place_column].last().unwrap(),
-            ) {
-                let card = cascades[pick_pos.0].remove(pick_pos.1);
-                cascades[place_column].push(card);
-            }
-        }
     }
 }
 
 fn can_place(picked: &str, place: &str) -> bool {
-    card_value(picked) == (card_value(place) - 1) && card_color(picked) != card_color(place)
+    (card_value(picked) + 1) == card_value(place) && card_color(picked) != card_color(place)
+        || place == "   "
+}
+
+fn can_move_to_foundation(card: &str, foundation_top: &str) -> bool {
+    (card_value(card) - 1) == card_value(foundation_top)
+        && card.chars().nth(2) == foundation_top.chars().nth(2)
+        || card_value(card) == 1 && card_value(foundation_top) == 0
 }
 
 fn card_value(s: &str) -> u8 {
